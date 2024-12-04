5d7484d15ca170f6f006a9b9d7a3d34aba7ebbe1
JsKro <stivencaro7@gmail.com>
2024-12-04 09:21:40 -0500

    TABLAS ACTUALIZADAS EN SU ESCTRUCTURA AMBINETE TEST Y DEV

---
+++ TABLAS_ESTRUCTURA_ACTUALIZADA_TEST/CIPO0152_TEST.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: rds-mysql-cima-dev-test.cluster-ccvz0yxqws4u.us-west-2.rds.amazonaws.com
+-- Tiempo de generación: 04-12-2024 a las 09:16:59
+-- Versión del servidor: 8.0.32
+-- Versión de PHP: 8.1.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `CIMADEV`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `CIPO0152`
+--
+
+CREATE TABLE `CIPO0152` (
+  `PROIDXXX` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'CODIGO DEL PRODUCTO (CIPO0141)',
+  `CLIIDXXX` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Nit cliente',
+  `IDPROXXX` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'ID DEL PROVEEDOR (CIPO0050)',
+  `DESIDXXX` int NOT NULL COMMENT 'ID DEL DESCRIPTOR (CIPO0037)',
+  `PREIDXXX` smallint NOT NULL COMMENT 'ID DE LA PRECONDICION',
+  `PRDESVAX` tinytext COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'DESCRIPTOR',
+  `REGUSRXX` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Usuario Creacion del Registro',
+  `REGFECXX` date NOT NULL COMMENT 'Fecha de Creacion del Registro',
+  `REGHORXX` time NOT NULL COMMENT 'Hora de Creacion del Registro',
+  `REGUSRMX` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Usuario de Modificacion',
+  `REGFECMX` date NOT NULL COMMENT 'Fecha de Modificacion del Registro',
+  `REGHORMX` time NOT NULL COMMENT 'Hora de Modificacion del Registro',
+  `REGESTXX` enum('ACTIVO','INACTIVO') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Estado del Registro',
+  `REGSTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Modificado'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='PRODUCTO VS DESCRIPCIONES MINIMAS';
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `CIPO0152`
+--
+ALTER TABLE `CIPO0152`
+  ADD PRIMARY KEY (`PROIDXXX`,`CLIIDXXX`,`IDPROXXX`,`DESIDXXX`),
+  ADD KEY `PROIDXXX` (`PROIDXXX`),
+  ADD KEY `CLIIDXXX` (`CLIIDXXX`),
+  ADD KEY `IDPROXXX` (`IDPROXXX`),
+  ADD KEY `DESIDXXX` (`DESIDXXX`),
+  ADD KEY `PROIDXXX_2` (`PROIDXXX`,`CLIIDXXX`,`IDPROXXX`,`DESIDXXX`,`PREIDXXX`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
