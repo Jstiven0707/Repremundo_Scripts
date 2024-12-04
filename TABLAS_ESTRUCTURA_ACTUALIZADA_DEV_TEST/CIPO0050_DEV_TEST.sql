10684c95f9a7e835aac7e04729755c9808aebd64
JsKro <stivencaro7@gmail.com>
2024-12-04 11:11:57 -0500

    Ajuste actualizacion tablas de diccionario

--- TABLAS_ESTRUCTURA_ACTUALIZADA_DEV_TEST/CIPO0050_DEV_TEST.sql
+++ TABLAS_ESTRUCTURA_ACTUALIZADA_DEV_TEST/CIPO0050_DEV_TEST.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: rds-mysql-cima-dev-test.cluster-ccvz0yxqws4u.us-west-2.rds.amazonaws.com
--- Tiempo de generación: 04-12-2024 a las 09:55:01
--- Versión del servidor: 8.0.32
--- Versión de PHP: 8.1.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `CIMADEV`
@@ -82,6 +64,4 @@ ALTER TABLE `CIPO0050`
   ADD KEY `FPIIDXXX` (`FPIIDXXX`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
--- TABLAS_ESTRUCTURA_ACTUALIZADA_DEV_TEST/CIPO0080_DEV_TEST.sql
+++ TABLAS_ESTRUCTURA_ACTUALIZADA_DEV_TEST/CIPO0080_DEV_TEST.sql
@@ -1,21 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: rds-mysql-cima-dev-test.cluster-ccvz0yxqws4u.us-west-2.rds.amazonaws.com
--- Tiempo de generación: 04-12-2024 a las 09:55:19
--- Versión del servidor: 8.0.32
--- Versión de PHP: 8.1.29
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `CIMADEV`
@@ -28,8 +10,8 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `CIPO0080` (
-  `RESIDXXX` int NOT NULL COMMENT 'ID DE LA DESCRIPCION',
-  `TIPPROID` int NOT NULL COMMENT 'ID DEL TIPO DE PRODUCTO',
+  `RESIDXXX` int (3) NOT NULL COMMENT 'ID DE LA DESCRIPCION',
+  `TIPPROID` int(4) NOT NULL COMMENT 'ID DEL TIPO DE PRODUCTO',
   `RESTIPBN` enum('','OBLIGATORIA','POR PRODUCTO') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'bandera que indica el nivel al que se aplica',
   `REGUSRXX` varchar(20) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL COMMENT 'Usuario Creacion del Registro',
   `REGFECXX` date NOT NULL COMMENT 'Fecha de Creacion del Registro',
@@ -54,6 +36,3 @@ ALTER TABLE `CIPO0080`
   ADD KEY `TIPPROID` (`TIPPROID`) USING BTREE;
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
