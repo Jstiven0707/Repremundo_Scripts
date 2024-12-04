3651b5f703f295751bef819c4214244b71c3e110
JsKro <stivencaro7@gmail.com>
2024-12-04 11:57:41 -0500

    Estructura Actualizada CIPO0152 hasta el 4 12 2024

--- TABLAS_ESTRUCTURA_ACTUALIZADA_DEV_TEST/CIPO0152_DEV_TEST.sql
+++ TABLAS_ESTRUCTURA_ACTUALIZADA_DEV_TEST/CIPO0152_DEV_TEST.sql
@@ -5,7 +5,7 @@
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `CIPO0152`
+-- Estructura  Ajustada  asi debe quedar Ajuste realizadas parte inferior  para la tabla `CIPO0152`
 --
 
 CREATE TABLE `CIPO0152` (
@@ -42,3 +42,23 @@ ALTER TABLE `CIPO0152`
  -- ADD KEY `PROIDXXX_2` (`PROIDXXX`,`CLIIDXXX`,`IDPROXXX`,`DESIDXXX`,`PREIDXXX`);
 COMMIT;
 
+-- MODIFY COLUM 
+ALTER TABLE CIPO0152
+MODIFY COLUMN `PROIDXXX` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'CODIGO DEL PRODUCTO (CIPO0141)';
+
+-- Indicice agregado 
+
+ALTER TABLE CIPO0152
+ADD KEY idx_PREIDXXX (`PREIDXXX`);
+
+-- drop index 
+ALTER TABLE `CIPO0152`
+DROP INDEX `PROIDXXX_2`;
+
+-- Modificar clave primaria compuesta
+
+ALTER TABLE `CIPO0152`
+  DROP PRIMARY KEY;
+  
+  ALTER TABLE `CIPO0152`
+  ADD PRIMARY KEY (`PROIDXXX`, `CLIIDXXX`, `IDPROXXX`, `DESIDXXX`, `PREIDXXX`);
